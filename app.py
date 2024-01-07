2a902ff2e66bd97e87954483f110fbc76c6b92af
梅宮 <euthanasia045@gmail.com>
2024-01-08 05:13:04 +0900

    いろいろ修正

---
+++ app.py
@@ -0,0 +1,17 @@
+import json
+
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    response = {}
+    response["pet"] = ["dog", "cat"]
+    return response
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
